172423d6fac6044fee6f4da0eaa3e40600cee9b7
TaiTan291 <dachnegfujing41@gmail.com>
2026-06-22 14:06:29 +0900

    feat flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Python 3.13 and uv development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              python313
+              uv
+            ];
+
+            # NixOSが提供するPythonを使用するため、uv自身によるPythonバイナリのダウンロードを無効化
+            env = {
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+
+            shellHook = ''
+              echo "Development shell activated"
+              echo "Python $(python --version)"
+              echo "uv $(uv --version)"
+            '';
+          };
+        });
+    };
+}
